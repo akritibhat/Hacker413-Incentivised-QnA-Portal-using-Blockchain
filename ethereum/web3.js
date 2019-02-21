42fec5f11c9f69ecfc2aa502dabc9c305dc6e213
Your Name <akriti.bhat28@gmail.com>
2019-02-20 23:34:04 -0500

    Hackher 413

---
+++ ethereum/web3.js
@@ -0,0 +1,19 @@
+import Web3 from 'web3';
+
+
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+
+    web3 = new Web3(window.web3.currentProvider);
+}
+else {
+
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/v3/3df3446a077546a8aeba10bd83191704'
+    );
+
+    web3 = new Web3(provider);
+}
+
+export default web3;
